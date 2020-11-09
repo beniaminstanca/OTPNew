8fef67ab98268cce5390b11d488120ef0b68f7b5
benys <beny.stanca97@gmail.com>
2020-11-09 03:52:01 +0200

    Update OtpTest.php

--- phpunit/tests/unit/OtpTest.php
+++ phpunit/tests/unit/OtpTest.php
@@ -11,10 +11,10 @@ class OtpTest extends \PHPUnit\Framework\TestCase
     }
 
     /** @test */
-    public function that_we_get_an_userId()
+    /*public function that_we_get_an_userId()
     {
         $otp = new \App\functions\Otp;
 
         $this->assertEquals(strlen($otp->GenerateOtp()), '6');
-    }
+    }*/
 }
